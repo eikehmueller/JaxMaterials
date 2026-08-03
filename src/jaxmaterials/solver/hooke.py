7e5b3e1a17391506d04774e2104385f90ee2f806
Eike Hermann Mueller <e.mueller@bath.ac.uk>
2026-08-03 14:45:48 +0200

    Further work on documentation

--- src/jaxmaterials/solver/hooke.py
+++ src/jaxmaterials/solver/hooke.py
@@ -5,22 +5,27 @@ Relates strain epsilon and stress sigma via :math:`\\sigma_{ij} = C_{ijkl}*\\eps
 
 from jax import numpy as jnp
 
-__all__ = ["compute_sigma_isotropic", "compute_sigma_anisotropic"]
+__all__ = [
+    "compute_sigma_isotropic",
+    "compute_sigma_anisotropic",
+    "compute_sigma_inelastic",
+]
 
 
 def compute_sigma_isotropic(epsilon, params):
     """Compute stress from strain for isotropic material
 
-    Returns :math:`\sigma_{ij} = C_{ijkl}\epsilon_{kl}` for an isotropic material characterised
-    by the two Lame parameters :math:`\\lambda` and :math:``\\mu``.
-    Voigt notation ``{00,11,22,01,02,12}`` is used for the stress and strain tensor.
+    Returns :math:`\\sigma_{ij} = C_{ijkl}\\epsilon_{kl}` for an isotropic material characterised
+    by the two Lame parameters :math:`\\lambda` and :math:`\\mu`.
+    Voigt notation ``{00,11,22,01,02,12}`` is used to index the stress and strain tensor.
 
     Parameters
     ==========
-    epsilon : numpy array
-        strain field
+    epsilon : `numpy.array <https://numpy.org/doc/stable/reference/generated/numpy.array.html>`_
+        strain field of shape ``(6,nx,ny,nz)``, using Voight notation for indexing.
     params : dict
-        Lame parameters, dictionary of the form ``{"lambda":lambda, "mu":mu}``
+        Lame parameters, dictionary of the form ``{"lambda":lambda, "mu":mu}`` where
+        ``lambda`` and ``mu`` are of shape ``(nx,ny,nz)``
     """
     tr_epsilon = epsilon[0, ...] + epsilon[1, ...] + epsilon[2, ...]
     sigma = 2 * params["mu"] * epsilon + params["lambda"] * jnp.stack(
@@ -32,30 +37,32 @@ def compute_sigma_isotropic(epsilon, params):
 def compute_sigma_anisotropic(epsilon, params):
     """Compute stress from strain for anisotropic material
 
-    Returns :math:``\\sigma_{ij} = C_{ijkl}\\epsilon_{kl}`` for an anisotropic material characterised
+    Returns :math:`\\sigma_{ij} = C_{ijkl}\\epsilon_{kl}` for an anisotropic material characterised
     by the 21 entries of the stiffness tensor :math:``C``.
 
-    Voigt notation ``{00,11,22,01,02,12}`` is used for the stress and strain tensor.
+    Voigt notation ``{00,11,22,01,02,12}`` is used to index the stress and strain tensor.
     The 21 independent entries of the stiffness tensor are given by
 
-    :math:
-    ..
-        C_{0}  = C_{00,00},   C_{1}  = C_{11,11},   C_{2}  = C_{22,22},
-        C_{3}  = C_{01,01},   C_{4}  = C_{02,02},   C_{5}  = C_{12,12},
-        C_{6}  = C_{00,11},   C_{7}  = C_{00,22},   C_{8}  = C_{11,22},
-        C_{9}  = C_{00,01},   C_{10} = C_{00,02},   C_{11} = C_{00,12},
-        C_{12} = C_{11,01},   C_{13} = C_{11,02},   C_{14} = C_{11,12},
-        C_{15} = C_{22,01},   C_{16} = C_{22,02},   C_{17} = C_{22,12},
-        C_{18} = C_{01,02},   C_{19} = C_{01,12},   C_{20} = C_{02,12},
-    ..
+    
+    .. math::
+      \\begin{aligned}
+        C_{0}  &= C_{00,00},&   C_{1}  &= C_{11,11},&   C_{2}  &= C_{22,22},\\\\
+        C_{3}  &= C_{01,01},&   C_{4}  &= C_{02,02},&   C_{5}  &= C_{12,12},\\\\
+        C_{6}  &= C_{00,11},&   C_{7}  &= C_{00,22},&   C_{8}  &= C_{11,22},\\\\
+        C_{9}  &= C_{00,01},&   C_{10} &= C_{00,02},&   C_{11} &= C_{00,12},\\\\
+        C_{12} &= C_{11,01},&   C_{13} &= C_{11,02},&   C_{14} &= C_{11,12},\\\\
+        C_{15} &= C_{22,01},&   C_{16} &= C_{22,02},&   C_{17} &= C_{22,12},\\\\
+        C_{18} &= C_{01,02},&   C_{19} &= C_{01,12},&   C_{20} &= C_{02,12}.
+      \\end{aligned}
 
     Parameters
     ==========
-    epsilon : numpy array
-        strain field :math:`\\epsilon`
+    epsilon : `numpy.array <https://numpy.org/doc/stable/reference/generated/numpy.array.html>`_
+        strain field :math:`\\epsilon` of shape ``(6,nx,ny,nz)``, using Voigt notation for indexing.
     params : dict
         dictionary of the form ``{"stiffness_tensor":stiffness_tensor}``
-        with vector representation of stiffness tensor :math:`C`
+        with vector representation of stiffness tensor :math:`C` which is passed as an array of
+        shape ``(21,nx,ny,nz)``
     """
     stiffness_tensor = params["stiffness_tensor"]
     sigma = jnp.stack(
@@ -109,11 +116,12 @@ def compute_sigma_inelastic(epsilon, params):
 
     Parameters
     ==========
-    epsilon : numpy array
+    epsilon : `numpy.array <https://numpy.org/doc/stable/reference/generated/numpy.array.html>`_
         strain field :math:`\\epsilon`
     params : dict
         Lame parameters :math:`\\lambda` and :math:`\\mu`, dictionary
-        of the form ``{"lambda":lambda, "mu":mu}``
+        of the form ``{"lambda":lambda, "mu":mu}``, where ``lambda`` and ``mu`` are
+        of shape ``(nx,ny,nz)``
     """
     tr_epsilon = epsilon[0, ...] + epsilon[1, ...] + epsilon[2, ...]
     sigma = 2 * params["mu"] / (
