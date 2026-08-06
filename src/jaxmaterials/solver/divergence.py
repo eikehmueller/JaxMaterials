f68f21984c9a529266d5f875ed4839e8596b40eb
Eike Hermann Mueller <e.mueller@bath.ac.uk>
2026-08-06 10:20:40 +0100

    Moved computation of relative divergence to separate module

---
+++ src/jaxmaterials/solver/divergence.py
@@ -0,0 +1,83 @@
+"""Relative convergence used for convergence tests
+
+See discussion in Section :ref:`sec:stopping_criterion` on how the relative divergence is used to check whether the Lippmann Schwinger solver has converged.
+"""
+
+from jax import numpy as jnp
+from jaxmaterials.solver.derivatives import backward_divergence
+
+__all__ = ["relative_divergence", "relative_divergence_fourier"]
+
+
+def relative_divergence(sigma, grid_spec):
+    """Relative divergence of stress :math:`\\sigma` in real space
+
+    Computes the ratio
+
+    .. math::
+
+        R(\\sigma) = \\frac{\\sqrt{\\langle\\|D^-\\cdot \\sigma\\|^2\\rangle}}{\\|\\langle \\sigma \\rangle\\|}
+
+    defined in Section :ref:`sec:stopping_criterion`.
+
+    Parameters
+    ==========
+    sigma : `numpy.array <https://numpy.org/doc/stable/reference/generated/numpy.array.html>`_
+        stress :math:`\\sigma`, array of shape ``(6,nx,ny,nz)``
+    grid_spec : :py:class:`jaxmaterials.common.GridSpec`
+        specification of computational grid
+
+    Returns
+    =======
+    ratio :math:`R(\\sigma)`
+    """
+    dsigma = backward_divergence(sigma, grid_spec)
+    dsigma_nrm = jnp.sqrt(jnp.sum(dsigma**2))
+    sigma_avg = jnp.mean(sigma, axis=[1, 2, 3])
+    sigma_avg_nrm = jnp.sqrt(
+        jnp.sum(sigma_avg[:3] ** 2) + 2 * jnp.sum(sigma_avg[3:] ** 2)
+    )
+    return dsigma_nrm / (jnp.sqrt(grid_spec.number_of_voxels) * sigma_avg_nrm)
+
+
+def relative_divergence_fourier(sigma_hat, xi):
+    """Relative divergence of stress :math:`\\sigma` in Fourier space
+
+    Computes the ratio
+
+    .. math::
+
+        \\widehat{R}(\\widehat{\\sigma}) = \\frac{\\sqrt{N\\langle\\|\\xi\\cdot \\widehat{\\sigma}\\|^2\\rangle}}{\\|\\widehat{\\sigma}_{\\boldsymbol{\\xi}=0}\\|}
+
+    Defined in Section :ref:`sec:stopping_criterion`. Here :math:`\\boldsymbol{\\xi}` are the Fourier vectors computed in :py:func:`jaxmaterials.solver.fourier.get_xi`.
+
+    Parameters
+    ==========
+    sigma_hat : `numpy.array <https://numpy.org/doc/stable/reference/generated/numpy.array.html>`_
+        stress :math:`\\sigma` in Fourier space, array of shape ``(6,nx,ny,nz)``
+    :arg xi : `numpy.array <https://numpy.org/doc/stable/reference/generated/numpy.array.html>`_
+        Fourier vectors, array of shape ``(6,nx,ny,nz)``
+
+    Returns
+    =======
+    ratio :math:`\\widehat{R}(\\widehat{\\sigma})`
+    """
+    dsigma_hat = jnp.stack(
+        [
+            xi[0, ...] * sigma_hat[0, ...]
+            + xi[1, ...] * sigma_hat[3, ...]
+            + xi[2, ...] * sigma_hat[4, ...],
+            xi[0, ...] * sigma_hat[3, ...]
+            + xi[1, ...] * sigma_hat[1, ...]
+            + xi[2, ...] * sigma_hat[5, ...],
+            xi[0, ...] * sigma_hat[4, ...]
+            + xi[1, ...] * sigma_hat[5, ...]
+            + xi[2, ...] * sigma_hat[2, ...],
+        ]
+    )
+    dsigma_nrm = jnp.sqrt(jnp.sum(jnp.abs(dsigma_hat) ** 2))
+    sigma_hat_zero = jnp.real(sigma_hat[:, 0, 0, 0])
+    sigma_hat_zero_nrm = jnp.sqrt(
+        jnp.sum(sigma_hat_zero[:3] ** 2) + 2 * jnp.sum(sigma_hat_zero[3:] ** 2)
+    )
+    return dsigma_nrm / sigma_hat_zero_nrm
---
+++ tests/test_divergence.py
@@ -0,0 +1,29 @@
+import numpy as np
+import jax
+
+from jaxmaterials.solver.fourier import get_xi
+from jaxmaterials.solver.divergence import (
+    relative_divergence,
+    relative_divergence_fourier,
+)
+
+jax.config.update("jax_enable_x64", True)
+
+from fixtures import grid_spec, rng
+
+
+def test_relative_divergence(grid_spec, rng):
+    """Verify that the relative divergence used for exit criterion is computed
+    consistently in real space and Fourier space
+
+    :arg grid_spec: specification of computational grid
+    :arg rng: random number generator
+    """
+
+    xi = get_xi(grid_spec)
+    sigma = rng.normal(size=(6, grid_spec.nx, grid_spec.ny, grid_spec.nz))
+    sigma_hat = np.fft.fftn(sigma, axes=[-3, -2, -1])
+    rel_div_real = relative_divergence(sigma, grid_spec)
+    rel_div_fourier = relative_divergence_fourier(sigma_hat, xi)
+    tolerance = 1.0e-5
+    assert abs((rel_div_real - rel_div_fourier) / rel_div_real) < tolerance
